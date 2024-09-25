8ed5da7323f9c21e5a3b92aa86eb94a79d9e5558
Mohamed Hussein <mfarrag19@gmail.com>
2024-09-25 15:31:36 +0300

    Navigation bar Complete

--- app/src/main/java/com/example/mvp_food_planner/homeActivity.java
+++ app/src/main/java/com/example/mvp_food_planner/homeActivity.java
@@ -1,19 +1,50 @@
 package com.example.mvp_food_planner;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.mvp_food_planner.View.FavFragment;
+import com.example.mvp_food_planner.View.HomeFragment;
+import com.example.mvp_food_planner.View.plannerFragment;
+import com.example.mvp_food_planner.databinding.ActivityHomeBinding;
 
 public class homeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment()); // set the default fragment for start
 
+        // set listener for the button
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.HomeMenu) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.FavMenu) {
+                replaceFragment(new FavFragment());
+            } else if (item.getItemId() == R.id.PlannerMenu) {
+                replaceFragment(new plannerFragment());
+            } else {
+                return false; // If no match, return false
+            }
+            return true; // Return true if an item was selected
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.commit();
     }
 }
